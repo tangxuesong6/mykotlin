99d1efb1802cd6a99ff795a683149bd7f31a4ab7
txs <16601136819@163.com>
2018-05-22 16:36:34 +0800

    初始化

---
+++ app/src/main/java/learn/song/com/mykt/MainActivity.kt
@@ -0,0 +1,16 @@
+package learn.song.com.mykt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * kotlin学习
+ * */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
